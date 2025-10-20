eb49f962337e73e6beb6a7ef74c38a6258fd77dc
Bielxrtt <gabri@D3F4ULT>
2025-10-20 15:37:16 -0300

    Adicionar letras

--- TermoOfIssac/TermoOfIsaac.Designer.cs
+++ TermoOfIssac/TermoOfIsaac.Designer.cs
@@ -31,14 +31,22 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TermoOfIsaac));
             button1 = new Button();
             label1 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
             button1.Font = new Font("IsaacGame", 7.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.Location = new Point(331, 399);
+            button1.Location = new Point(291, 385);
             button1.Name = "button1";
-            button1.Size = new Size(53, 38);
+            button1.Size = new Size(38, 31);
             button1.TabIndex = 1;
             button1.Text = "A";
             button1.UseVisualStyleBackColor = true;
@@ -53,12 +61,100 @@
             label1.TabIndex = 2;
             label1.Text = "label1";
             // 
+            // button2
+            // 
+            button2.Font = new Font("IsaacGame", 7.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(335, 385);
+            button2.Name = "button2";
+            button2.Size = new Size(38, 31);
+            button2.TabIndex = 3;
+            button2.Text = "A";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("IsaacGame", 7.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(379, 385);
+            button3.Name = "button3";
+            button3.Size = new Size(38, 31);
+            button3.TabIndex = 4;
+            button3.Text = "A";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("IsaacGame", 7.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.Location = new Point(423, 385);
+            button4.Name = "button4";
+            button4.Size = new Size(38, 31);
+            button4.TabIndex = 5;
+            button4.Text = "A";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("IsaacGame", 7.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button5.Location = new Point(476, 385);
+            button5.Name = "button5";
+            button5.Size = new Size(38, 31);
+            button5.TabIndex = 6;
+            button5.Text = "A";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("IsaacGame", 7.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button6.Location = new Point(520, 385);
+            button6.Name = "button6";
+            button6.Size = new Size(38, 31);
+            button6.TabIndex = 7;
+            button6.Text = "A";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("IsaacGame", 7.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button7.Location = new Point(564, 385);
+            button7.Name = "button7";
+            button7.Size = new Size(38, 31);
+            button7.TabIndex = 8;
+            button7.Text = "A";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("IsaacGame", 7.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button8.Location = new Point(608, 385);
+            button8.Name = "button8";
+            button8.Size = new Size(38, 31);
+            button8.TabIndex = 9;
+            button8.Text = "A";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Font = new Font("IsaacGame", 7.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button9.Location = new Point(652, 385);
+            button9.Name = "button9";
+            button9.Size = new Size(38, 31);
+            button9.TabIndex = 10;
+            button9.Text = "A";
+            button9.UseVisualStyleBackColor = true;
+            // 
             // TermoOfIsaac
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             ClientSize = new Size(1002, 561);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
             Controls.Add(label1);
             Controls.Add(button1);
             Name = "TermoOfIsaac";
@@ -72,5 +168,13 @@
         #endregion
         private Button button1;
         private Label label1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
     }
 }
